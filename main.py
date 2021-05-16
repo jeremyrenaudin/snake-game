7b2d9a9b908112e03daed87248d424ae195369bb
jeremyrenaudin <jeremyrenaudin@hotmail.fr>
2021-05-16 17:57:07 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+# initialize the screen settings
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+# initialize the game
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+# define keys that the screen will detect and their related actions to execute
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
+
+# launch the game
+game_is_on = True
+while game_is_on:
+    # update the screen every 0.1 second after each snake's move
+    screen.update()
+    time.sleep(0.1)
+
+    # call .move() method on snake object at each screen update
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with walls
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on=False
+        scoreboard.game_over()
+
+    # Detect collision between head and tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on=False
+            scoreboard.game_over()
+        
+# exit the game on click
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,59 @@
+from turtle import Turtle
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    """Model the snake"""
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        """Create the snake by defining its length, shape, color and starting position"""
+        for position in STARTING_POSITION:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        """Build a segment used to build the snake"""
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        """Add a new segment on the snake's tail"""
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        """Set the snake in movement"""
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """Ask the snake to move up. Impossible if the snake is currently moving down."""
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """Ask the snake to move down. Impossible if the snake is currently moving up."""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """Ask the snake to move left. Impossible if the snake is currently moving right."""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """Ask the snake to move right. Impossible if the snake is currently moving left."""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
